fcf05608d6770d75292a47178fa06da0d700ddb7
LeoBourret <bourretleo@hotmail.fr>
2022-04-30 17:10:58 +0000

    calls of enable_if fixed

--- containers/utils/utils.hpp
+++ containers/utils/utils.hpp
@@ -8,6 +8,33 @@
 # include <typeinfo>
 # include <iostream>
 
+static class nullptr_t
+{
+    public:
+        /*
+        ** @brief For conversion to any type
+        ** of null non-member pointer.
+        */
+        template<class T>
+        operator T*() const { return (0); }
+
+        /*
+        ** @brief For conversion to any type of null
+        ** member pointer.
+        */
+        template<class C, class T>
+        operator T C::*() const { return (0); }
+
+    private:
+
+        /*
+        ** @brief It's imposible to get an address of
+        ** a nullptr.
+        */
+        void operator&() const;
+
+} u_nullptr = {};
+
 namespace ft
 {
     class bidirectional_iterator_tag { };
